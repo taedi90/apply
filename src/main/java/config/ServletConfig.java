bb89ccd04b92fc056b9bb34c357721a02ad0d6df
taedi90 <taedi90@gmail.com>
2021-12-16 16:08:46 +0900

    프로젝트 초기화
    - 콘솔창 한글 깨짐 이슈

---
+++ src/main/java/config/ServletConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"controller"})
+public class ServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry)  {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        registry.viewResolver(bean);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
+//        registry.addResourceHandler("/img/**").addResourceLocations("/resources/img/");
+//        registry.addResourceHandler("/upload/**").addResourceLocations("/resources/upload/");
+//        registry.addResourceHandler("/se2/**").addResourceLocations("/resources/se2/");
+    }
+
+}
